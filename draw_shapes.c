6a4e31d711294f5ab9d4b5fdbee7e1a287f5bd6d
MayaSabrinna9 <msgarcia5@miners.utep.edu>
2025-02-12 06:34:16 -0700

    Added 't' command to draw an arrow

--- draw_shapes.c
+++ draw_shapes.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "draw.h"
 
+
 /* Prints a size x size square whose left col is at startCol */
 void print_square(int leftCol, int size)
 {
@@ -25,4 +26,32 @@ void print_triangle(int leftCol, int size)
     putchar('\n');
   }
 }
+/* Prints an arrow of specified size whose left edge is at col leftCol */
+void print_arrow(int leftCol, int size)
+{
+  unsigned char arrow_bitmap[7] = {
+    0b00100,  //   *
+    0b01110,  //  ***
+    0b10101,  // * * *
+    0b00100,  //   *
+    0b00100,  //   *
+    0b00100,  //   *
+    0b00100   //   *
+  };
 
+  for (int row = 0; row < 7 * size; row++) {  // Scale height
+    int col;
+    int bitmap_row = arrow_bitmap[row / size];  // Adjust row scaling
+    int minCol = leftCol, maxCol = leftCol + (5 * size);
+
+    for (col = 0; col < minCol; col++) putchar(' ');  // Left padding
+
+    for (int bit = 4; bit >= 0; bit--) {  // Process each bit in the 5x7 pattern
+      int pixel = (bitmap_row >> bit) & 1;  // Extract pixel
+      for (int scale_x = 0; scale_x < size; scale_x++) {  // Scale width
+	putchar(pixel ? '*' : ' ');  // Print '*' for 1, space for 0
+      }
+    }
+    putchar('\n');
+  }
+}
